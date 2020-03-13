64358da00d70df8d9a7ec97703bbd5fc3953c27f
kevin <kevin@kdoran.com>
2020-03-13 13:31:21 +0900

    Adding support for deep_fill layers. Trying out a small such model. Trying 64x64 input.

--- gen_network_vis.py
+++ gen_network_vis.py
@@ -7,22 +7,18 @@ import matplotlib.pyplot as plt
 
 #pr = cProfile.Profile()
 #pr.enable()
-max_step = 100
+max_step = 1000
+DIR = './out/experiments/2/11'
 dataset_outdir_pairs = (
-#    ('./out/experiments/2/2/instrumentation.nc', './out/experiments/2/2/network_vis'),
-#    ('./out/experiments/2/2/d_instrumentation.nc', './out/experiments/2/2/network_vis_gradient'),
-    ('./out/experiments/2/2/step_instrumentation.nc',
-        './out/experiments/2/2/network_vis_step'),
+    (f'{DIR}/instrumentation.nc', f'{DIR}/network_vis'),
+    (f'{DIR}/d_instrumentation.nc', f'{DIR}/network_vis_gradient'),
+    (f'{DIR}/step_instrumentation.nc', f'{DIR}/network_vis_step'),
 )
-
-#ds = xr.open_dataset('./out/experiments/2/2/instrumentation.nc')
-#net_vis.save_network_step_with_density(ds, 0, './out/experiments/2/2/network_vis', './out/experiments/2/2/network_vis/network_density.png')
 
 
 for ds_path, out_dir in dataset_outdir_pairs:
-    ds = xr.open_dataset(ds_path)
-    net_vis.network_density_plot(ds, f'{out_dir}/network_density.png')
+    ds = xr.open_dataset(ds_path, cache=False)
     pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True) 
-    #net_vis.print_network(ds, out_dir, max_step)
+    net_vis.print_network(ds, out_dir, max_step)
 #pr.disable()
 #pr.print_stats(sort='cumtime')
--- src/dip/experiments/two/interactive_training.py
+++ src/dip/experiments/two/interactive_training.py
@@ -1,20 +1,21 @@
 from typing import List
 import re
 import logging
+import collections
 
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
+from neuralgym.ops.layers import resize
 import xarray as xr
 
 import dip.image_io
 import dip.network_vis as net_vis
 
 
-OUT_DIR = 'out/experiments/2/2'
-NET_NAME = 'mini_model'
+OUT_DIR = 'out/experiments/2/11'
 
 
 class MyAdam(tf.train.AdamOptimizer):
@@ -76,7 +77,8 @@ def plot_as_tf_summary(x):
 
 
 def create_optimizer():
-    lr = 0.005
+    #lr = 0.005
+    lr = 0.002
     optimizer = MyAdam(lr, epsilon=0.01)
     return optimizer, lr
 
@@ -87,7 +89,117 @@ def l2_loss(out_img, target_img):
     return loss
 
 
-def build_model(xin, filters=None):
+def gen_conv(x, cnum, ksize, stride=1, rate=1, name='conv',
+             padding='same', activation=tf.nn.elu, training=True):
+    """Define conv for generator.
+
+    Args:
+        x: Input.
+        cnum: Channel number.
+        ksize: Kernel size.
+        Stride: Convolution stride.
+        Rate: Rate for or dilated conv.
+        name: Name of layers.
+        padding: Default to SYMMETRIC.
+        activation: Activation function after convolution.
+        training: If current graph is for training or inference, used for bn.
+
+    Returns:
+        tf.Tensor: output
+
+    """
+    layer = tf.keras.layers.Conv2D(
+        cnum, ksize, stride, dilation_rate=rate,
+        activation=activation, padding=padding, name=name)
+    #if cnum == 3 or activation is None:
+        # conv for output
+        #return x, layer
+    #x, y = tf.split(x, 2, 3)
+    #x = activation(x)
+    #y = tf.nn.sigmoid(y)
+    #x = x * y
+    x = layer(x)
+    return x, layer
+
+
+def gen_deconv(x, cnum, ksize, stride, rate, name='upsample', padding='same', training=True):
+    """Define deconv for generator.
+    The deconv is defined to be a x2 resize_nearest_neighbor operation with
+    additional gen_conv operation.
+
+    Args:
+        x: Input.
+        cnum: Channel number.
+        name: Name of layers.
+        training: If current graph is for training or inference, used for bn.
+
+    Returns:
+        tf.Tensor: output
+
+    """
+    # Just using these as options to keep the signature the same as gen_conv.
+    assert ksize == 3
+    assert stride == 1
+    assert rate == None
+    with tf.variable_scope(name):
+        x = resize(x, func=tf.image.resize_nearest_neighbor)
+        x, layer = gen_conv(
+            x, cnum, 3, 1, name=name+'_conv', padding=padding,
+            training=training)
+    return x, layer
+
+def build_encoder_decoder_model(xin):
+    LayerConf = collections.namedtuple('LayerConf', 
+            ['func', 'num_filters', 'filter_size', 'stride', 'rate', 'name'])
+    filter_factor = 20#48
+    layer_conf = [
+        #           func   |  num filters    | filter size | stride | rate | name
+        LayerConf(gen_conv,   1+filter_factor,       5,         1,       1,    'conv1'),
+        #LayerConf(gen_conv,   2*filter_factor,       3,         2,       1,    'conv2_downsample'),
+        LayerConf(gen_conv,   2+filter_factor,       3,         1,       1,    'conv2'),           # 32x32
+        #LayerConf(gen_conv,   4*filter_factor,       3,         2,       1,    'conv4_downsample'),     
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       1,    'conv3'),           # 16x16
+        LayerConf(gen_conv,   4+filter_factor,       3,         2,       1,    'conv4_downsample'),     
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       1,    'conv5'),
+        LayerConf(gen_conv,   4+filter_factor,       3,         2,       1,    'conv6_downsample'),     
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       2,    'conv7_atrous'), 
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       4,    'conv8_atrous'),
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       8,    'conv9_atrous'),
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,      16,    'conv10_atrous'),
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       1,    'conv11'),
+        LayerConf(gen_conv,   4+filter_factor,       3,         1,       1,    'conv12'),
+        LayerConf(gen_deconv, 2+filter_factor,       3,         1,       None, 'conv13_upsample'),
+        LayerConf(gen_conv,   2+filter_factor,       3,         1,       1,    'conv14'),
+        LayerConf(gen_deconv, 1+filter_factor,       3,         1,       None, 'conv15_upsample'),
+        LayerConf(gen_conv,     filter_factor,      3,         1,       1,    'conv16')]
+        #LayerConf(gen_conv,   1,                     3,         1,       1,    'conv17')]
+    keras_layers = []
+    layer_inputs = []
+    x = xin
+    for c in layer_conf:
+        layer_inputs.append(x)
+        x, l = c.func(x, *list(c)[1:])
+        keras_layers.append(l)
+    layer_inputs.append(x)
+    x, l = gen_conv(x, 1, 3, 1, activation='tanh', name='conv17')
+    keras_layers.append(l)
+
+    layer_instrumentation = []
+    for i, l in enumerate(keras_layers):
+        kernel, bias = l.variables
+        if 'kernel' not in kernel.name:
+            raise Exception('Unexpected layer variable: {kernel.name}')
+        if 'bias' not in bias.name:
+            raise Exception('Unexpected layer variable: {bias.name}')
+        layer_instrumentation.append(net_vis.InstrumentationData.Layer(
+            layer_inputs[i], kernel, bias))
+
+    # Add the last layer (output) instrumentation. 
+    layer_instrumentation.append(
+            net_vis.InstrumentationData.Layer(x, None, None))
+    return x, layer_instrumentation
+
+def build_model(xin):
     """Builds a very simple convolutional neural network.
 
     The output will have the same dimensions as the input.
@@ -95,14 +207,13 @@ def build_model(xin, filters=None):
     Returns: the output tensor
     """
     x = xin
-    net_name = NET_NAME
     padding = 'SAME'
-    def relu6(x): 
+    def relu6(x):
         return tf.keras.activations.relu(x, max_value=6.0)
 
     # Layer descriptions.
     num_filters = [10, 10, 10, 1]
-    filter_size = [3,   3,  3, 1]
+    filter_size = [3,  3, 3, 1]
     activations = [relu6, relu6, relu6, 'tanh']
     assert len(num_filters) == len(filter_size) == len(activations)
 
@@ -141,7 +252,7 @@ def build_model(xin, filters=None):
     return x, layer_instrumentation
 
 
-def run(input_img_path, target_img_path, filters=None):
+def run(input_img_path, target_img_path):
     input_img = dip.image_io.load_img_greyscale(input_img_path)
     target_img = dip.image_io.load_img_greyscale(target_img_path)
     # Add batch dimension.
@@ -152,7 +263,10 @@ def run(input_img_path, target_img_path, filters=None):
             tf.convert_to_tensor(target_img, dtype=tf.float32),
             axis=0)
     tf.train.get_or_create_global_step()
-    out, layers = build_model(input_img, filters)
+    # Build simple model:
+    out, layers = build_model(input_img)
+    # Build deep_fill model (first half)
+    #out, layers = build_encoder_decoder_model(input_img)
     loss = l2_loss(out, target_img)
     loss += 0.001 * tf.add_n([tf.nn.l2_loss(p) for p in tf.trainable_variables()])
     optimizer, learning_rate = create_optimizer()
@@ -162,11 +276,13 @@ def run(input_img_path, target_img_path, filters=None):
     psnr = tf.image.psnr(out, target_img, max_val=PIXEL_RANGE)
 
     steps_per_img_save = 1
-    max_step = 600
+    max_step = 1000
 
     instrumentation = net_vis.InstrumentationData(layers)
     d_instrumentation = instrumentation.as_derivative(loss)
-    step_instrumentation = net_vis.InstrumentationData(layers)
+    step_instrumentation = net_vis.InstrumentationData(
+            [net_vis.InstrumentationData.Layer(l.layer_in, l.kernel, l.bias) for
+                l in layers])
     trainable_var_set = set((v for v in tf.trainable_variables()))
     for i in range(len(step_instrumentation.layers)):
         if step_instrumentation.layers[i].kernel in trainable_var_set:
@@ -208,24 +324,11 @@ def run(input_img_path, target_img_path, filters=None):
     return psnr_val[0]
 
 
-def main2():
-    res = []
-    for i in range(20, 50):
-        filters = [i]
-        psnr = run(input_img_path='./resources/greyscale_noise_16x16.png',
-                    target_img_path='./resources/greyscale_patternA_16x16.png',
-                    filters=filters)
-        res.append(psnr)
-    print(res)
-
-
 def main():
     #run(input_img_path='./resources/greyscale_noise_16x16.png',
     #    target_img_path='./resources/greyscale_vertical_bar_16x16.png')
-    run(input_img_path='./resources/statue1_256_empty_mask.png',
-        target_img_path='./resources/statue1_256_noise.jpg')
-    #run(target_img_path='./resources/greyscale_noise_16x16.png',
-    #    input_img_path='./resources/greyscale_patternA_16x16.png')
+    run(input_img_path='./resources/greyscale_noise_64x64.png',
+        target_img_path='./resources/statue1_64_greyscale.png')
 
 
 if __name__ == '__main__':
